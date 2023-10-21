101d25157e8a861286024badc42a70f6d4d69ad2
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-10-21 15:40:52 +0100

    set up and installed cors, express, pg, body parser

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+app.use(cors({ AllowedHeaders: ["Content-Type", "Authorization"] }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const { Pool } = require("pg");
+
+const db = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
+});
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
