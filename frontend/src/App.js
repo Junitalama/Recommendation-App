dbdb9790075567e9a667151100a2a3bb7e183b52
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-10-26 18:50:55 +0100

    style added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MovieList from './Components/MovieList';
+import BookList from './Components/BookList';
+import VideoList from './Components/VideoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Recommendations</h3>
+      <div className="App">
+        <MovieList />
+        <BookList />
+        <VideoList />
+      </div>
     </div>
   );
 }
