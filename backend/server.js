b3dab2cbe053925a2c6d23ac0c48dabee730e98f
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-10-26 18:50:28 +0100

    get endpoint added

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 app.use(cors({ AllowedHeaders: ["Content-Type", "Authorization"] }));
 app.use(bodyParser.urlencoded({ extended: false }));
-
 const { Pool } = require("pg");
 
 const db = new Pool({
@@ -17,7 +16,7 @@ const db = new Pool({
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  db.query("select * from recommendation")
+  db.query("select * from recommendation_cross join recommendation on(recommendation_cross.id = recommendation.rec_id) join users on (recommendation_cross.id = users.user_id) join moods on (recommendation_cross.id = moods.mood_id)")
     .then((result) => res.json(result.rows))
     .catch((err) => res.send(err));
 });
